b6d8200daa9dc5ca5a0ab975586667335cba6965
Matt Mower <self@mattmower.com>
2008-07-20 11:16:56 +0100

    Initial commit of project Elysium

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Elysium
+//
+//  Created by Matt Mower on 20/07/2008.
+//  Copyright LucidMac Software 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
---
+++ src/ElysiumDocument.h
@@ -0,0 +1,15 @@
+//
+//  ElysiumDocument.h
+//  Elysium
+//
+//  Created by Matt Mower on 20/07/2008.
+//  Copyright LucidMac Software 2008 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface ElysiumDocument : NSDocument
+{
+}
+@end
