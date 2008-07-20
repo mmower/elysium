f711811aedb89eef0f6e10d58935d63f467fff87
Matt Mower <self@mattmower.com>
2008-07-20 20:41:44 +0100

    Added ELConfig, a nestable configuration storage

---
+++ src/ELConfig.h
@@ -0,0 +1,30 @@
+//
+//  ELConfig.h
+//  Elysium
+//
+//  Created by Matt Mower on 20/07/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ELConfig : NSObject {
+  ELConfig              *parent;
+  NSMutableDictionary   *data;
+}
+
+- (id)initWithData:(NSMutableDictionary *)data;
+- (id)initWithParent:(ELConfig *)parent data:(NSMutableDictionary *)data;
+
+- (BOOL)hasValueForKey:(NSString *)key;
+
+- (id)valueForKey:(NSString *)key;
+- (void)setValue:(id)value forKey:(NSString *)key;
+
+- (int)integerForKey:(NSString *)key;
+- (void)setInteger:(int)value forKey:(NSString *)key;
+
+- (float)floatForKey:(NSString *)key;
+- (void)setFloat:(float)value forKey:(NSString *)key;
+
+@end
---
+++ src/ELConfig.m
@@ -0,0 +1,64 @@
+//
+//  ELConfig.m
+//  Elysium
+//
+//  Created by Matt Mower on 20/07/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import "ELConfig.h"
+
+@implementation ELConfig
+
+- (id)initWithData:(NSMutableDictionary *)_data {
+  return [self initWithParent:nil data:_data];
+}
+
+- (id)initWithParent:(ELConfig *)_parent data:(NSMutableDictionary *)_data {
+  if( self = [super init] ) {
+    parent = _parent;
+    data   = _data;
+  }
+  
+  return self;
+}
+
+- (BOOL)hasValueForKey:(NSString *)_key {
+  return [data objectForKey:_key] != nil;
+}
+
+- (id)valueForKey:(NSString *)_key {
+  id value = [data objectForKey:_key];
+
+  if( value != nil ) {
+    return value;
+  }
+  
+  if( parent == nil ) {
+    return nil;
+  }
+  
+  return [parent valueForKey:_key];
+}
+
+- (void)setValue:(id)_value forKey:(NSString *)_key {
+  [data setObject:_value forKey:_key];
+}
+
+- (int)integerForKey:(NSString *)_key {
+  return [[self valueForKey:_key] integerValue];
+}
+
+- (void)setInteger:(int)_value forKey:(NSString *)_key {
+  [data setObject:[NSNumber numberWithInt:_value] forKey:_key];
+}
+
+- (float)floatForKey:(NSString *)_key {
+  return [[self valueForKey:_key] floatValue];
+}
+
+- (void)setFloat:(float)_value forKey:(NSString *)_key {
+  [data setObject:[NSNumber numberWithFloat:_value] forKey:_key];
+}
+
+@end
