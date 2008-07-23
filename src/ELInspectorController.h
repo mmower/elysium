f5468caa51c45c8aac09be998bb1d2f07fb7ae55
Matt Mower <self@mattmower.com>
2008-07-24 00:11:55 +0100

    Added outline of an inspector

---
+++ src/ELInspectorController.h
@@ -0,0 +1,16 @@
+//
+//  ELInspectorController.h
+//  Elysium
+//
+//  Created by Matt Mower on 22/07/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ELInspectorController : NSWindowController {
+
+}
+
+@end
---
+++ src/ELInspectorController.m
@@ -0,0 +1,14 @@
+//
+//  ELInspectorController.m
+//  Elysium
+//
+//  Created by Matt Mower on 22/07/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import "ELInspectorController.h"
+
+
+@implementation ELInspectorController
+
+@end
