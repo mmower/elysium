7f8d106e758f7ba7239c0c42cada47d52f29b5fc
Matt Mower <self@mattmower.com>
2008-07-23 16:47:24 +0100

    Hex radius is now determined by the width of the layer view

--- src/ELLayerView.m
+++ src/ELLayerView.m
@@ -12,9 +12,6 @@
 
 #import "ELRegularPolygon.h"
 
-#define HEX_RADIUS 25.0
-#define HEX_OFFSET 37.5
-
 @implementation ELLayerView
 
 - (id)initWithFrame:(NSRect)frame {
@@ -27,13 +24,18 @@
 
 - (void)drawRect:(NSRect)rect {
   NSRect bounds = [self bounds];
-  
+  NSLog( @"Width = %f", bounds.size.width );
+  CGFloat radius = bounds.size.width / 27;
+  NSLog( @"Hex Radius = %f", radius );
+  CGFloat offset = ( 3 * radius ) / 2;
+  NSLog( @"Offset = %f", offset );
+    
   [[NSColor redColor] set];
   [NSBezierPath fillRect:bounds];
   
   [[NSColor whiteColor] set];
   
-  CGFloat hexDiameter = 2 * sqrt( 0.75 * pow( HEX_RADIUS, 2 ) );
+  CGFloat hexDiameter = 2 * sqrt( 0.75 * pow( radius, 2 ) );
   NSLog( @"Hex diameter = %f", hexDiameter );
   
   NSBezierPath *hexGrid = [NSBezierPath bezierPath];
@@ -41,10 +43,10 @@
   for( int cols = 0; cols < HTABLE_COLS; cols++ ) {
     for( int rows = 0; rows < HTABLE_ROWS; rows ++ ) {
       hexCentre = NSMakePoint(
-                    HEX_OFFSET + (cols * ( (3 * HEX_RADIUS ) / 2 ) ),
-                    HEX_OFFSET + (rows * hexDiameter) + ( cols % 2 == 0 ? (hexDiameter / 2) : 0 )
+                    offset + (cols * ( (3 * radius ) / 2 ) ),
+                    offset + (rows * hexDiameter) + ( cols % 2 == 0 ? (hexDiameter / 2) : 0 )
                     );
-      [hexGrid appendHexagonWithCentre:hexCentre radius:HEX_RADIUS];
+      [hexGrid appendHexagonWithCentre:hexCentre radius:radius];
     }
   }
   
