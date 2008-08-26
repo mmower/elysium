ddaa77c1c028d316d3576b2014a415a770ba94ae
Matt Mower <self@mattmower.com>
2008-08-26 11:40:05 +0100

    Added basic palette nib and window controller

---
+++ src/ELPaletteController.h
@@ -0,0 +1,16 @@
+//
+//  ELPaletteController.h
+//  Elysium
+//
+//  Created by Matt Mower on 26/08/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ELPaletteController : NSWindowController {
+
+}
+
+@end
---
+++ src/ELPaletteController.m
@@ -0,0 +1,14 @@
+//
+//  ELPaletteController.m
+//  Elysium
+//
+//  Created by Matt Mower on 26/08/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import "ELPaletteController.h"
+
+
+@implementation ELPaletteController
+
+@end
