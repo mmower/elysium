5e6cb24627757b7bba1ebdea5d6709de8b9b03cc
Matt Mower <self@mattmower.com>
2008-09-08 22:23:19 +0100

    Added outline probability generator classes.

---
+++ src/pgen/ELProbabilityGenerator.m
@@ -0,0 +1,13 @@
+//
+//  ELProbabilityGenerator.m
+//  Elysium
+//
+//  Created by Matt Mower on 08/09/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import "ELProbabilityGenerator.h"
+
+@implementation ELProbabilityGenerator
+
+@end
---
+++ src/pgen/ELSinusoidalProbabilityGenerator.h
@@ -0,0 +1,16 @@
+//
+//  ELSinusoidalProbabilityGenerator.h
+//  Elysium
+//
+//  Created by Matt Mower on 08/09/2008.
+//  Copyright 2008 LucidMac Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "ELEnvelopeProbabilityGenerator.h"
+
+@interface ELSinusoidalProbabilityGenerator : ELEnvelopeProbabilityGenerator {
+}
+
+@end
