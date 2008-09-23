3cd37ae885fe9e1f3866303b662b7476b03f64b9
Matt Mower <self@mattmower.com>
2008-09-24 00:28:20 +0100

    Removed last default oscillator from Player. Users should setup their own.

--- src/datamodel/ELPlayer.m
+++ src/datamodel/ELPlayer.m
@@ -40,21 +40,6 @@
     durationKnob   = [[ELFloatKnob alloc] initWithName:@"duration" floatValue:0.5];
     transposeKnob  = [[ELIntegerKnob alloc] initWithName:@"transpose" integerValue:0];
     
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/1" variance:1.0 period:1.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/1" variance:0.5 period:1.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/2" variance:1.0 period:2.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/2" variance:0.5 period:2.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/5" variance:1.0 period:5.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/5" variance:0.5 period:5.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/10" variance:1.0 period:10.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/10" variance:0.5 period:10.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/15" variance:1.0 period:15.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/15" variance:0.5 period:15.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"1/30" variance:1.0 period:30.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/30" variance:0.5 period:30.0]];
-    [filters addObject:[[ELOscillator alloc] initWithName:@"1/60" function:@"Sine" variance:1.0 period:60.0]];
-    // [filters addObject:[[ELSinusoidalOscillator alloc] initWithName:@"0.5/60" variance:0.5 period:60.0]];
-    
     nextLayerNumber = 1;
     showNotes       = NO;
   }
