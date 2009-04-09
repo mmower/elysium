d03ca8ad59bbecaede5dfdefaaadd5edf39b856b
Rune Botten <rbotten@gmail.com>
2009-04-09 15:17:01 +0200

    Allow changing selection with arrow keys. The way this works might be a little counter-intuitive at times, so a different handling of this is under experiment. Perhaps let the user set prefs on how the selection moves.

--- src/mainview/ELLayerWindowController.m
+++ src/mainview/ELLayerWindowController.m
@@ -10,6 +10,8 @@
 #import "ELLayerWindowController.h"
 
 #import "ELLayer.h"
+#import "ELCell.h"
+#import "Elysium.h"
 
 @implementation ELLayerWindowController
 
@@ -50,6 +52,71 @@
   return [NSString stringWithFormat:@"%@ - %@ (Channel %d)", displayName, [[self layer] layerId], [[[self layer] channelDial] value]];
 }
 
+#pragma mark Move selection with arrow keys
+
+-(void)selectCellAtColumn:(int)column row:(int)row
+{
+	ELCell * nextSelection = (ELCell*)[[layerView dataSource] hexCellAtColumn:column row:row];
+	[nextSelection makeCurrentSelection];
+}
+
+// Deliberately not using Direction and [cell neighbour]
+- (void)moveRight:(id)sender
+{
+	ELCell *currentSelection = [layerView selectedCell];
+	
+	if([currentSelection column] < HTABLE_MAX_COL ) {
+		[self selectCellAtColumn:[currentSelection column] + 1
+							 row:[currentSelection row]];
+	} else if ([currentSelection row] > 0){
+		[self selectCellAtColumn:0
+							 row:[currentSelection row] -1];
+	} else {
+		[self selectCellAtColumn:0
+							 row:HTABLE_MAX_ROW];
+	}
+}
+- (void)moveLeft:(id)sender
+{
+	ELCell *currentSelection = [layerView selectedCell];
+	
+	if([currentSelection column] > 0) {
+		[self selectCellAtColumn:[currentSelection column] - 1
+							 row:[currentSelection row]];
+	} else if([currentSelection column] == 0
+			  && [currentSelection row] == HTABLE_MAX_ROW) {
+		[self selectCellAtColumn:HTABLE_MAX_COL
+							 row:0];
+	} else {
+		[self selectCellAtColumn:HTABLE_MAX_COL
+							 row:[currentSelection row]  + 1];
+	}
+}
+- (void)moveUp:(id)sender
+{
+	ELCell *currentSelection = [layerView selectedCell];
+	
+	if([currentSelection row] == HTABLE_MAX_ROW && [currentSelection column] == HTABLE_MAX_COL) {
+		[self selectCellAtColumn:0 row:0];
+	} else {
+		//[[currentSelection neighbour:(Direction) N] makeCurrentSelection];
+		// Adding 1 to row will let us continue on bottom of next column
+		[self selectCellAtColumn:[currentSelection column]
+							 row:[currentSelection row] + 1];
+	}
+}
+- (void)moveDown:(id)sender
+{
+	ELCell *currentSelection = [layerView selectedCell];
+	
+	if([currentSelection row] == 0 && [currentSelection column] == 0) {
+		[self selectCellAtColumn:HTABLE_MAX_COL
+							 row:HTABLE_MAX_ROW];
+	} else {
+		[self selectCellAtColumn:[currentSelection column]
+							 row:[currentSelection row] - 1];
+	}
+}
 
 #pragma mark NSKeyValueObserving protocol
 
