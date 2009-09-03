d4dfc98277af43e758e2c64833755d6e51c03128
Matt Mower <self@mattmower.com>
2009-09-03 10:09:17 +0100

    Simplified bindings of the note inspector using bind-macro methods.

--- src/inspector/ELNoteInspectorViewController.m
+++ src/inspector/ELNoteInspectorViewController.m
@@ -22,36 +22,16 @@
 - (void)awakeFromNib {
   [self bindControl:@"enabled"];
   
-  [self bindControl:@"p"];
-  [self bindMode:@"p"];
-  [self bindOsc:@"p"];
+  [self bindDial:@"p"];
+  [self bindDial:@"gate"];
+  [self bindDial:@"velocity"];
+  [self bindDial:@"emphasis"];
   
-  [self bindControl:@"gate"];
-  [self bindMode:@"gate"];
-  [self bindOsc:@"gate"];
+  // [self bindControl:@"tempoSync"];
   
-  [self bindControl:@"velocity"];
-  [self bindMode:@"velocity"];
-  [self bindOsc:@"velocity"];
-  
-  [self bindControl:@"emphasis"];
-  [self bindMode:@"emphasis"];
-  [self bindOsc:@"emphasis"];
-  
-  [self bindControl:@"tempoSync"];
-  
-  [self bindControl:@"noteLength"];
-  [self bindMode:@"noteLength"];
-  [self bindOsc:@"noteLength"];
-  
-  [self bindControl:@"ghosts"];
-  [self bindMode:@"ghosts"];
-  [self bindOsc:@"ghosts"];
-  
-  [self bindControl:@"override"];
-  
+  [self bindDial:@"noteLength"];
+  [self bindDial:@"ghosts"];
   [self bindTriadControl];
-  
   [self bindOverrideControls];
   
   [self bindScript:@"willRun"];
@@ -68,30 +48,15 @@
                         withKeyPath:@"selection.overrideDial.value"
                             options:[NSDictionary dictionaryWithObject:NSNegateBooleanTransformerName forKey:NSValueTransformerNameBindingOption]];
   
-  [self bindMode:@"chan1Override"];
-  [self bindControl:@"chan1Override"];
-  [self bindOsc:@"chan1Override"];
-  [self bindMode:@"chan2Override"];
-  [self bindControl:@"chan2Override"];
-  [self bindOsc:@"chan2Override"];
-  [self bindMode:@"chan3Override"];
-  [self bindControl:@"chan3Override"];
-  [self bindOsc:@"chan3Override"];
-  [self bindMode:@"chan4Override"];
-  [self bindControl:@"chan4Override"];
-  [self bindOsc:@"chan4Override"];
-  [self bindMode:@"chan5Override"];
-  [self bindControl:@"chan5Override"];
-  [self bindOsc:@"chan5Override"];
-  [self bindMode:@"chan6Override"];
-  [self bindControl:@"chan6Override"];
-  [self bindOsc:@"chan6Override"];
-  [self bindMode:@"chan7Override"];
-  [self bindControl:@"chan7Override"];
-  [self bindOsc:@"chan7Override"];
-  [self bindMode:@"chan8Override"];
-  [self bindControl:@"chan8Override"];
-  [self bindOsc:@"chan8Override"];
+  [self bindControl:@"override"];
+  [self bindDial:@"chan1Override"];
+  [self bindDial:@"chan2Override"];
+  [self bindDial:@"chan3Override"];
+  [self bindDial:@"chan4Override"];
+  [self bindDial:@"chan5Override"];
+  [self bindDial:@"chan6Override"];
+  [self bindDial:@"chan7Override"];
+  [self bindDial:@"chan8Override"];
 }
 
 @end
